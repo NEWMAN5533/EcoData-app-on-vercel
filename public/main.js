a59a4e05fb74c4ddd91447ff640cc372c7cd35fd
NEWMAN5533 <ilyasahmed553341@gmail.com>
2026-04-08 14:00:27 +0000

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -18,7 +18,7 @@ const API_BASE = (() => {
   if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
     return "http://localhost:3000";
   }
-  return "https://cheapdata-bundles.vercel.app/"; // your deployed backend
+  return "https://ecodata-app.vercel.app"; // your deployed backend
 })();
 
 
